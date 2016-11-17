9529f6e1b3571ae5311aa2712d4db657d1357612
David Nguyen <dvnguyen@gmail.com>
2016-11-16 18:08:42 -0600

    Change AJAX call  option to

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,7 +2,7 @@ $(function() {
 
   // your code will go here
   $.ajax('https://www.codeschool.com/users/743893.json', {
-    dataType: 'json',
+    dataType: 'jsonp',
     success: function(response) {
 
     }
