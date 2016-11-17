13379f1065a9bb66a13bc57096e6a3932daac37e
David Nguyen <dvnguyen@gmail.com>
2016-11-16 18:07:20 -0600

    Make an AJAX call.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,10 @@
 $(function() {
 
   // your code will go here
+  $.ajax('https://www.codeschool.com/users/743893.json', {
+    dataType: 'json',
+    success: function(response) {
 
+    }
+  });
 });
