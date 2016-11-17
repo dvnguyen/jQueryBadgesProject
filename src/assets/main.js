32d9e50766d34e568ea327e951dff15b31096fda
David Nguyen <dvnguyen@gmail.com>
2016-11-16 18:43:00 -0600

    Complete steps 6 to 9.

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,24 @@ $(function() {
   $.ajax('https://www.codeschool.com/users/743893.json', {
     dataType: 'jsonp',
     success: function(response) {
+      $.each(response.courses.completed, function(index, course) {
+        var item  = $('<div></div>');
+        var title = $('<h3>' + course.title + '</h3>');
+        var image = $('<img>').attr('src', course.badge);
+        var link  = $('<a>See Course</a>');
 
+        link
+          .attr('href', course.url)
+          .attr('target', '_blank')
+          .addClass('btn btn-primary');
+
+        item
+          .addClass('course')
+          .append(title)
+          .append(image)
+          .append(link)
+          .appendTo('#badges');
+      });
     }
   });
 });
